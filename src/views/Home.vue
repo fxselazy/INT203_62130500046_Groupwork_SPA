110154813808372fa52a6ef4f4596c27c4ff6597
fxselazy <nirawat.fuse@mail.kmutt.ac.th>
2021-04-16 18:44:02 +0700

    62130500046 create homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="home">
+  <div class="home flex justify-center items-center w-screen h-screen">
+    <div class="flex flex-col items-center space-y-24">
+      <h1 class="text-6xl font-bold">Timer Website</h1>
+      <div class="flex space-x-8 text-2xl">
+        <router-link to="">
+          <button class="bg-gray-300 py-2 px-6">
+            your timer
+          </button>
+        </router-link>
+        <router-link to="">
+          <button class="bg-green-400 py-2 px-6">
+            add timer
+          </button>
+        </router-link>
+      </div>
+      <div class="h-8"></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-}
+  name: "Home",
+};
 </script>
