0d7107d2c74181ba289ec678c0a09a929a6cc461
fxselazy <nirawat.fuse@mail.kmutt.ac.th>
2021-04-16 19:16:49 +0700

    62130500046 create newTimerPage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home flex justify-center items-center w-screen h-screen">
+  <div class="home flex justify-center items-center w-full h-screen">
     <div class="flex flex-col items-center space-y-24">
       <h1 class="text-6xl font-bold">Timer Website</h1>
       <div class="flex space-x-8 text-2xl">
@@ -8,7 +8,7 @@
             your timer
           </button>
         </router-link>
-        <router-link to="">
+        <router-link to="/newtimer">
           <button class="bg-green-400 py-2 px-6">
             add timer
           </button>
---
+++ src/views/NewTimerPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-24">
+      <add-timer-form></add-timer-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewTimerPage"
+}
+</script>
+
+<style>
+
+</style>
