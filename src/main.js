c5d0066a5c01493fe55c45c82b4cb8531a0ee1ca
fxselazy <nirawat.fuse@mail.kmutt.ac.th>
2021-04-16 19:16:04 +0700

    62130500046 create AddTimerForm component

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import AddTimerForm from '@/components/AddTimerForm.vue';
 
-createApp(App).use(router).mount('#app')
+createApp(App).component("add-timer-form", AddTimerForm)
+.use(router)
+.mount('#app')
